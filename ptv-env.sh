34efcb552fd711d985ad5d0f766b31af782f84ea
Robert Dyer <rdyer@unl.edu>
2022-01-27 19:50:55 -0600

    unquote arguments passed to CMD

--- ptv-env.sh
+++ ptv-env.sh
@@ -6,4 +6,4 @@ grep "unalias" bin/pitivi-env >/dev/null
 [[ $? -eq 1 ]] && echo "unalias ptvenv" >> bin/pitivi-env && echo "git restore bin/pitivi-env" >> bin/pitivi-env
 
 #source bin/pitivi-env
-exec "$@"
+exec $@
