5d67177a3f06b1f52a5a947d4b85db2280f16761
Robert Dyer <rdyer@unl.edu>
2023-01-30 12:51:22 -0600

    port map the debugging port 5678

--- dev-pitivi.sh
+++ dev-pitivi.sh
@@ -8,7 +8,14 @@ if [ ! "$(docker ps -q -f name=pitivi-dev)" ]; then
     fi
 
     docker pull soft261/pitivi-dev:latest
-    docker run -it --privileged --name pitivi-dev -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/pitivi/pitivi-dev:/pitivi-dev soft261/pitivi-dev:latest
+    docker run \
+        -it --privileged \
+        --name pitivi-dev \
+        -e DISPLAY=$IP:0 \
+        -p 5678:5678 \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v ~/pitivi/pitivi-dev:/pitivi-dev \
+        soft261/pitivi-dev:latest
 else
     echo "pitivi-dev container already running as: "
     docker ps -q -f name=pitivi-dev
